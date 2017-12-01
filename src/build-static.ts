63cc0597cc6f0151b65ef29d719621a2497a30d2
Matthias Kunnen <matthias.kunnen@gmail.com>
2017-12-01 12:44:05 +0100

    Created first stable version

--- src/build-static.ts
+++ src/build-static.ts
@@ -1,2 +1,110 @@
+import * as cheerio from 'cheerio';
+import * as fs from 'fs';
+import * as mkdirp from 'mkdirp';
+import * as path from 'path';
+import * as puppeteer from 'puppeteer';
+
 export namespace Statisfy {
+
+    export interface StatisfyConfigInterface {
+
+        /**
+         * The directory to place the generated files in.
+         * @default
+         */
+        directory?: string;
+
+        /**
+         * An absolute URL pointing to the host to statisfy.
+         */
+        host: string;
+
+        /**
+         * An array of routes to statisfy.
+         */
+        routes: string[];
+
+        /**
+         * Use this to disable the sandbox setting of puppeteer. This can be useful when
+         * encountering errors in a docker environment.
+         * @default true
+         */
+        sandBox?: boolean;
+    }
+
+    export function generateStaticHtml(config: StatisfyConfigInterface) {
+
+        const puppeteerArgs: Array<string> = [];
+
+        if (config.sandBox === false) {
+            puppeteerArgs.push(...[
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+            ]);
+        }
+
+        (async () => {
+            const browser = await puppeteer.launch({args: puppeteerArgs});
+
+            for (const route of config.routes) {
+                const fullRoute = config.host + route;
+                console.log(`Statisfying ${fullRoute}`);
+
+                let html: any;
+                let success = false;
+                let tryCounter = 0;
+
+                while (!success && tryCounter <= 3) {
+                    try {
+                        const page = await browser.newPage();
+                        await page.goto(fullRoute);
+                        html = await page.evaluate(() => document.documentElement.outerHTML);
+                        await page.close();
+                        success = true;
+                    } catch (e) {
+                        console.warn(`Could not evaluate ${fullRoute} in`
+                            + ` try ${tryCounter++}.`);
+                        console.warn(`Error: ${e}`);
+                    }
+                }
+
+                if (!success) {
+                    console.error(`Could not evaluate ${fullRoute} in ${tryCounter} tries.`);
+                    continue;
+                }
+
+                const dom = cheerio.load(html);
+                dom('script').each((index, item) => {
+                    if ('src' in item.attribs && !item.attribs.src.startsWith('http')) {
+                        dom(item).remove();
+                    }
+                });
+
+                dom('base').attr('href', config.host);
+
+                const editedDom = dom.html();
+                writeFile(
+                    `${config.directory}/${route ? route : 'index'}.html`,
+                    editedDom,
+                    err => {
+                        if (err) {
+                            console.log(err);
+                        }
+                    },
+                );
+            }
+
+            await browser.close();
+        })();
+    }
+
+    function writeFile(filePath: string, contents: any, cb: (err) => void) {
+        mkdirp(path.dirname(filePath), err => {
+            if (err) {
+                return cb(err);
+            }
+
+            fs.writeFile(filePath, contents, cb);
+        });
+    }
 }
